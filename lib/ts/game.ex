857ec6f1dc5ab20c396a0707a1548af8c68de211
Kang Zhang <piecehealth@sina.com>
2020-11-05 10:26:10 +0800

    asia map

--- lib/ts/game.ex
+++ lib/ts/game.ex
@@ -1,10 +1,24 @@
 defmodule Ts.Game do
+  @bg "battle-ground"
+  @normal "normal"
+
   @map %{
     # Asia
-    "n_korea" => {"asia", "asia", 3, "battle-ground", 1650, 150},
-    "s_korea" => {"asia", "asia", 3, "battle-ground", 1650, 300},
-    "japan" => {"asia", "asia", 4, "battle-ground", 1800, 450},
-    "philippines" => {"asia", "southeast-asia", 2, "normal", 1650, 500}
+    "n_korea" => {"asia", "asia", 3, @bg, 2170, 200},
+    "s_korea" => {"asia", "asia", 3, @bg, 2200, 300},
+    "japan" => {"asia", "asia", 4, @bg, 2300, 400},
+    "taiwan" => {"asia", "asia", 3, @normal, 2150, 500},
+    "philippines" => {"asia", "southeast-asia", 2, @normal, 2200, 650},
+    "indonesia" => {"asia", "southeast-asia", 1, @normal, 2170, 870},
+    "australia" => {"asia", "asia", 4, @normal, 2170, 1000},
+    "malaysia" => {"asia", "southeast-asia", 2, @normal, 1900, 760},
+    "vietnam" => {"asia", "southeast-asia", 1, @normal, 1980, 650},
+    "thailand" => {"asia", "southeast-asia", 2, @bg, 1840, 650},
+    "laos_cambodia" => {"asia", "southeast-asia", 1, @normal, 1900, 550},
+    "burma" => {"asia", "southeast-asia", 2, @normal, 1760, 530},
+    "india" => {"asia", "asia", 3, @bg, 1590, 510},
+    "pakistan" => {"asia", "asia", 2, @bg, 1440, 450},
+    "afghanistan" => {"asia", "asia", 2, @normal, 1440, 350}
   }
 
   def map, do: @map
