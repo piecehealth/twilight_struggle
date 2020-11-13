864c282d8601ad776dc0fca9d236409808e82866
Kang Zhang <piecehealth@sina.com>
2020-11-13 14:20:12 +0800

    update config

--- config/prod.exs
+++ config/prod.exs
@@ -21,15 +21,7 @@ config :logger, level: :info
 # To get SSL working, you will need to add the `https` key
 # to the previous section and set your `:url` port to 443:
 #
-config :ts, TsWeb.Endpoint,
-  url: [host: "ts.gigalixirapp.com", port: 443],
-  https: [
-    port: 443,
-    cipher_suite: :strong,
-    keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-    certfile: System.get_env("SOME_APP_SSL_CERT_PATH"),
-    transport_options: [socket_opts: [:inet6]]
-  ]
+config :ts, TsWeb.Endpoint, url: [host: "ts.gigalixirapp.com", port: 443]
 
 #
 # The `cipher_suite` is set to `:strong` to support only the
